5d3cf6b23cc84d01ace2ce1a846862a4e892e880
kasulemoses <80149379+kasulemoses@users.noreply.github.com>
2026-01-02 13:18:10 -0800

    Fix compileSdk configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.plana"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.plana"
